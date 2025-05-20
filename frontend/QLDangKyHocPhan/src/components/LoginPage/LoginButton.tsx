696111127aa9c761949a93b14b23e8f719792142
Kh4nG <nguyen2004xuankhang@gmail.com>
2025-05-21 00:21:13 +0700

    bug : don't have pointer cursor when hover

--- frontend/QLDangKyHocPhan/src/components/LoginPage/LoginButton.tsx
+++ frontend/QLDangKyHocPhan/src/components/LoginPage/LoginButton.tsx
@@ -7,7 +7,7 @@ export const LoginButton = ({ onClick, disabled }: Props) => (
   <button
     onClick={onClick}
     disabled={disabled}
-    className="bg-blue-600 text-white py-2 rounded hover:bg-blue-700 disabled:opacity-50 w-full"
+    className="cursor-pointer bg-blue-600 text-white py-2 rounded hover:bg-blue-700 disabled:opacity-50 w-full"
   >
     Đăng nhập
   </button>
