bdfe974af4505ca0c23180dd71d4e3a7e059e79b
minhthinh145 <minhthinh0384360149@gmail.com>
2025-05-20 19:49:30 +0700

    feat : ADd BateOfBirth to TaiKhoan

--- QLDangKyHocPhan/QLDangKyHocPhan/Models/Taikhoan.cs
+++ QLDangKyHocPhan/QLDangKyHocPhan/Models/Taikhoan.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.Data;
 
 namespace QLDangKyHocPhan.Models;
 
@@ -8,6 +9,7 @@ public partial class Taikhoan : IdentityUser
 {
     public string TenDangNhap { get; set; }
     public string LoaiTaiKhoan { get; set; }
+    public DateTime? DateOfBirth { get; set; }
     public virtual ICollection<Sinhvien> Sinhviens { get; set; }
     public virtual ICollection<Giangvien> Giangviens { get; set; }
 }
