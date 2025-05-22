308a119652c45d8e3a97100dba0cba3e15c85ca5
Kh4nG <nguyen2004xuankhang@gmail.com>
2025-05-22 08:34:16 +0700

    bug : fix show/hide password function

--- frontend/QLDangKyHocPhan/src/components/LoginPage/LoginButton.tsx
+++ frontend/QLDangKyHocPhan/src/components/LoginPage/LoginButton.tsx
@@ -7,7 +7,7 @@ export const LoginButton = ({ onClick, disabled }: Props) => (
   <button
     onClick={onClick}
     disabled={disabled}
-    className="w-full bg-indigo-600 text-white py-3 rounded-full
+    className="mt-7 w-full bg-indigo-600 text-white py-3 rounded-full
                        font-medium hover:bg-indigo-700 focus:outline-none focus:ring-2 
                        focus:ring-offset-2 focus:ring-indigo-500 transition-colors hover-scale"
   >
--- frontend/QLDangKyHocPhan/src/components/LoginPage/ShowPassword.tsx
+++
@@ -1,6 +0,0 @@
-export const ShowPasswordButton = () => (
-  <div className="mb-4 flex items-center">
-    <input id="showPassword" type="checkbox" className="cursor-pointer mr-2" />
-    <label htmlFor="showPassword">Hiển thị mật khẩu</label>
-  </div>
-);
